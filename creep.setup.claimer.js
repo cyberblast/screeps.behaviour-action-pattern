dc4688acc181a6301c7d416de850aa04badf722d
cyberblast <git@cyberblast.org>
2016-07-18 13:45:23 +0200

    Claimer Spawn validation fixed

--- creep.setup.claimer.js
+++ creep.setup.claimer.js
@@ -3,23 +3,15 @@ var setup = new MODULES.creep.Setup();
 setup.type = 'claimer';
 setup.multiBody = [CLAIM, MOVE];
 setup.minAbsEnergyAvailable = 650;
-setup.maxMulti = 1;
+setup.maxMulti = 2;
 setup.globalMeasurement = true;
 setup.minEnergyAvailable = function(){
     return 0.9;
 }
-setup.isValidSetup = function(spawn){ return false;
-    var room = spawn.room;    
-    var globalClaimers = _.countBy(Memory.creeps, 'setup').claimer;
-    var flag = Game.flags['Claim'];
-    var valid = (room.energyAvailable >= this.defaultBodyCosts && 
-        room.energyAvailable >= (room.energyCapacityAvailable * this.minEnergyAvailable(spawn)) && 
-        flag != null && (globalClaimers == null || globalClaimers < 0) 
-    );
-    return valid;
+setup.maxCount = function(spawn){
+    return _.filter(Game.flags, {'color': FLAG_COLOR.claim }).length;
 }
-setup.maxCount = function(spawn){ return 0; }, 
 setup.maxWeight = function(spawn){
-    return 700;
+    return 1300;
 }
 module.exports = setup;
