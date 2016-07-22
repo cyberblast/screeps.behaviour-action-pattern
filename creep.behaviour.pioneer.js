d1418e3432dbf716362cee318583c3e35ee55d86
cyberblast <git@cyberblast.org>
2016-07-22 22:11:24 +0200

    Introducing Pioneers

--- creep.behaviour.pioneer.js
+++ creep.behaviour.pioneer.js
@@ -2,9 +2,11 @@ var behaviour = new MODULES.creep.Behaviour();
 
 behaviour.run = function(creep) {    
     var flag = _.find(Game.flags, {'color': FLAG_COLOR.settle });   
-    if( flag.room && flag.rooms.spawns && flag.rooms.spawns.length > 0 )
+    if( flag.room && flag.room.spawns && flag.room.spawns.length > 0 )
         flag.remove();
-        
+    if( flag.room && flag.room.constructionSites.count == 0 )
+        flag.room.createConstructionSite(flag, STRUCTURE_SPAWN);
+
     if( flag && flag.room.name != creep.room.name ){
         this.assignActionWithTarget(creep, MODULES.creep.action.settling);
         return;
