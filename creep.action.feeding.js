f2067a235e31d443cdbaf4da31cdcd45e4cd50e8
cyberblast <git@cyberblast.org>
2016-07-28 10:47:58 +0200

    Increased feeding when hive empty

--- creep.action.feeding.js
+++ creep.action.feeding.js
@@ -9,11 +9,11 @@ action.isValidTarget = function(target){
     return ( (target != null) && (target.energy != null) && (target.energy < target.energyCapacity) );
 };   
 action.isAddableAction = function(creep){
-    return (!creep.room.activities[this.name] || creep.room.activities[this.name] < creep.room.maxPerJob);
+    return (!creep.room.activities[this.name] || creep.room.activities[this.name] < (creep.room.maxPerJob * (target.room.relativeEnergyAvailable < HIVE_ENERGY_URGENT ? 2 : 1)) );
 };
 action.isAddableTarget = function(target){ 
     return (!target.creeps || !target.creeps[this.maxPerTargetType] || 
-    target.creeps[this.maxPerTargetType].length < (this.maxPerTarget * (target.room.relativeEnergyAvailable < HIVE_ENERGY_URGENT ? 2 : 1) ));
+    target.creeps[this.maxPerTargetType].length < this.maxPerTarget);
 };
 action.maxPerTarget = 1;
 action.newTarget = function(creep){
