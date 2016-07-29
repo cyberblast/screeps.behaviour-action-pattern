6ac223f6173121466846d6ab913ef810d9094a09
cyberblast <git@cyberblast.org>
2016-07-29 17:29:41 +0200

    increasing melee

--- creep.setup.melee.js
+++ creep.setup.melee.js
@@ -3,14 +3,14 @@ var setup = new MODULES.creep.Setup();
 setup.type = 'melee';
 setup.multiBody = [TOUGH, TOUGH, ATTACK, MOVE, MOVE, MOVE]; 
 setup.minAbsEnergyAvailable = 250;
-setup.maxMulti = 3;
+setup.maxMulti = 4;
 setup.globalMeasurement = true;
 setup.minControllerLevel = 2;
 setup.minEnergyAvailable = function(){
     return 0.8;
 }
 setup.maxCount = function(spawn){
-    return _.filter(Game.flags, FLAG_COLOR.defense.filter).length;
+    return _.filter(Game.flags, FLAG_COLOR.defense.filter).length*2;
 }
 setup.maxWeight = function(spawn){
     return null; // no evaluation
