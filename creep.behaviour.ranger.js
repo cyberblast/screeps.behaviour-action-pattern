7d568ee7cdaf4d689788e45ae85cb1e638bfa121
cyberblast <git@cyberblast.org>
2016-07-30 20:18:39 +0200

    Include in invasion

--- creep.behaviour.ranger.js
+++ creep.behaviour.ranger.js
@@ -2,25 +2,28 @@ var behaviour = new Creep.Behaviour('ranger');
 
 behaviour.run = function(creep) {
     var assignment;
-    if( creep.room.situation.invasion )
+    if( creep.room.controller.my && creep.room.situation.invasion )
         assignment = this.assignAction(creep, Creep.action.defending);
     else {
-        if( creep.memory.action ){
-            if( !this.validateMemoryAction(creep) ){
-                creep.room.activities[creep.memory.action]--;
-                creep.unregisterTarget(creep.Target);
-                creep.memory.action = null;
-                creep.memory.target = null;
-                creep.action = null;
-                creep.target = null;
+        var flag = _.find(Game.flags, FLAG_COLOR.destroy.filter) || _.find(Game.flags, FLAG_COLOR.invade.filter);
+        if( flag ) assignment = this.assignAction(creep, Creep.action.invading);
+        else {
+            if( creep.memory.action ){
+                if( !this.validateMemoryAction(creep) ){
+                    creep.room.activities[creep.memory.action]--;
+                    creep.unregisterTarget(creep.Target);
+                    creep.memory.action = null;
+                    creep.memory.target = null;
+                    creep.action = null;
+                    creep.target = null;
 
-                assignment = false;
-            } else assignment = true;
+                    assignment = false;
+                } else assignment = true;
+            }
+            if(!assignment)
+                assignment = this.assignAction(creep, Creep.action.guarding);
         }
-        if(!assignment)
-            assignment = this.assignAction(creep, Creep.action.guarding);
     }
-    
     if( !assignment ) this.assignAction(creep, Creep.action.idle);
     
     if( creep.action ) creep.action.step(creep);
