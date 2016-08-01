fc8d4c2cc5b32ddd8be963baee4b16e6dd5b09ea
cyberblast <git@cyberblast.org>
2016-08-01 20:47:01 +0200

    Fixed bug with empty storage

--- creep.setup.privateer.js
+++ creep.setup.privateer.js
@@ -14,7 +14,7 @@ setup.maxCount = function(spawn){
     return _.filter(Game.flags, FLAG_COLOR.invade.exploit.filter).length * 5;
 }
 setup.maxWeight = function(spawn){
-    return null; 
+    return _.filter(Game.flags, FLAG_COLOR.invade.exploit.filter).length * 4000; 
 }
 
 module.exports = setup;
