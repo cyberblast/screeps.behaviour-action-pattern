ffc038cee3c2c31a2bae00781d4764abda5783bb
cyberblast <git@cyberblast.org>
2016-08-04 14:16:21 +0200

    Fixing ERR_BUSY
    Error ocurred when calling Spawn.prototype.loop while it was already spawning.

--- spawn.js
+++ spawn.js
@@ -1,6 +1,7 @@
 var mod = {
     extend: function(){
         Spawn.prototype.loop = function(){
+            if( this.spawning ) return;
             var self = this;
             [Creep.setup.worker, 
                 Creep.setup.melee,
@@ -10,18 +11,16 @@ var mod = {
                 Creep.setup.pioneer, 
                 Creep.setup.privateer
             ].forEach(function(set) {
-                if( !self.busy && set.isValidSetup(self) ){
+                if( set.isValidSetup(self) ){
                     var params =  set.buildParams(self);
-                    
                     var newName = self.createCreep(params.parts, params.id, params);
-                    if( params.id == newName ){
-                        self.busy = true;
+                    if( params.id == newName || ERROR_CODE(newName) === undefined ){ // other name assigned?
                         Population.registerCreepSetup(self.room, params.setup, params.cost);
-                    }
-                    
-                    if( params.id == newName || ERROR_CODE(newName) === undefined ){
                         if(DEBUG) console.log( DYE(CRAYON.system, self.name + ' &gt; ') + DYE(CRAYON.birth, 'Good morning ' + newName + '!') );
-                    } else console.log( DYE(CRAYON.system, self.name + ' &gt; ') + DYE(CRAYON.error, 'Offspring failed: ' + ERROR_CODE(newName)) );
+                        return;
+                    } 
+                    
+                    console.log( DYE(CRAYON.system, self.name + ' &gt; ') + DYE(CRAYON.error, 'Offspring failed: ' + ERROR_CODE(newName)) );
                 }
             });
         };
