b63c022ba9bfcdc1ac50beb0b9f7f235382436e0
cyberblast <git@cyberblast.org>
2016-08-29 21:21:11 +0200

    Fixed another bug
    which caused the creeps to seach for a new flag each loop.

--- creep.action.guarding.js
+++ creep.action.guarding.js
@@ -5,7 +5,7 @@ action.isAddableTarget = function(){ return true; };
 action.newTarget = function(creep){     
     var flag = FlagDir.find(FLAG_COLOR.defense, creep.pos, false, 
         FlagDir.rangeMod, 240, creep.data.creepType);
-    if( creep.action && creep.action.name == 'guarding' && creep.flag && creep.flag.pos.roomName == creep.pos.roomName )
+    if( creep.action && creep.action.name == 'guarding' && creep.flag )
         return creep.flag;
     if( flag ) Population.registerCreepFlag(creep, flag);
     return flag;
