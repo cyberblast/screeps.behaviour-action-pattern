e0b0317e26e10d74e3321d00167a158b5098bdc7
cyberblast <git@cyberblast.org>
2016-09-06 00:37:44 +0200

    better ranger

--- creep.setup.ranger.js
+++ creep.setup.ranger.js
@@ -1,7 +1,7 @@
 var setup = new Creep.Setup('ranger');
 setup.multiBody = [RANGED_ATTACK, MOVE]; 
 setup.minAbsEnergyAvailable = 200;
-setup.maxMulti = 8;
+setup.maxMulti = 10;
 setup.globalMeasurement = true;
 setup.minControllerLevel = 5;
 setup.minEnergyAvailable = function(spawn){
