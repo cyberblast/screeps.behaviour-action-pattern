8ffef604290abf20d80a9ed128bc1757e406f67c
cyberblast <git@cyberblast.org>
2016-09-05 15:21:08 +0200

    Upgrader to uncharge & upgrade in the same loop

--- creep.action.upgrading.js
+++ creep.action.upgrading.js
@@ -12,6 +12,32 @@ action.newTarget = function(creep){
     return ( creep.room.controller && creep.room.controller.my) ? creep.room.controller : null;
 };
 action.work = function(creep){
+    if( creep.data.creepType == "upgrader" && creep.carry.energy < creep.carryCapacity*0.5 ) {
+        let cont = creep.pos.findInRange(creep.room.chargeablesOut, 1, {
+            filter: function(c){ 
+                return c && c.store && c.store[RESOURCE_ENERGY] > 0;
+            }
+        });
+        if( cont ) creep.withdraw(cont[0], RESOURCE_ENERGY);
+    } 
     return creep.upgradeController(creep.target);
+}; 
+action.step = function(creep){
+    if(CHATTY) creep.say(this.name, SAY_PUBLIC);
+    let range = creep.pos.getRangeTo(creep.target);
+    if( range <= this.targetRange ) {
+        var workResult = this.work(creep);
+        if( workResult != OK ) {
+            if( DEBUG ) logErrorCode(creep, workResult);
+            creep.data.actionName = null;
+        }
+    } 
+    if( creep.data.creepType == "upgrader" && range <= this.targetRange ){ // stay at container
+        let cont = creep.pos.findInRange(creep.room.chargeablesOut, 1 );
+        if( cont.length > 0 ) return;
+    }
+    if( range > 1 )
+        this.drive(creep, creep.target.pos, range);
 };
+
 module.exports = action;
