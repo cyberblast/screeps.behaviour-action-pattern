742ea55e3d3d7aaa664633c357c975c30ba27b37
SirLovi <stanekdo@gmail.com>
2016-09-07 10:53:26 +0200

    Repairing fix

--- creep.action.repairing.js
+++ creep.action.repairing.js
@@ -5,7 +5,7 @@ action.isValidAction = function(creep){
 };
 action.isValidTarget = function(target){
     return ( target != null && target.hits != null && 
-    target.hits < target.hitsMax;
+    target.hits < target.hitsMax);
 };   
 action.newTarget = function(creep){
     var that = this;
