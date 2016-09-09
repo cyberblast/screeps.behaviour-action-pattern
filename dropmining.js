2d8b03ff091407397b70566daab2f9408e803998
cyberblast <git@cyberblast.org>
2016-09-09 07:16:05 +0200

    hauler.nextAction override unnecessary
    picking is already added at first (if !invasion)

--- dropmining.js
+++ dropmining.js
@@ -2,7 +2,6 @@ var mod = {
     extend: function(){   
         Creep.action.dropping = this.droppingAction();
         Creep.behaviour.miner.nextAction = this.minerBehaviourNextAction;
-        Creep.behaviour.hauler.nextAction = this.haulerBehaviourNextAction;
         Creep.setup.hauler.maxCount = () => 2;
     }, 
     droppingAction: function() {
@@ -38,41 +37,6 @@ var mod = {
         }
         if( creep.room.population && creep.room.population.typeCount['hauler'] && creep.room.population.typeCount['hauler'] > 0 ) {
             priority.unshift(Creep.action.dropping);
-        }
-        for(var iAction = 0; iAction < priority.length; iAction++) {
-            var action = priority[iAction];
-            if(action.isValidAction(creep) && 
-                action.isAddableAction(creep) && 
-                action.assign(creep)) {
-                    return;
-            }
-        }
-    }, 
-    haulerBehaviourNextAction: function(creep){
-        let priority;
-        if( creep.carry.energy == 0 ) { 
-            priority = [
-                Creep.action.uncharging, 
-                Creep.action.withdrawing, 
-                Creep.action.idle];
-        }    
-        else {	  
-            priority = [
-                Creep.action.picking, 
-                Creep.action.feeding, 
-                Creep.action.charging, 
-                Creep.action.fueling, 
-                Creep.action.storing, 
-                Creep.action.idle];
-            if( creep.room.controller && creep.room.controller.ticksToDowngrade < 2000 ) { // urgent upgrading 
-                priority.unshift(Creep.action.upgrading);
-            }
-        }
-        if( !creep.room.situation.invasion && _.sum(creep.carry) < creep.carryCapacity) {
-            priority.unshift(Creep.action.picking);
-        }
-        if( _.sum(creep.carry) > creep.carry.energy ) {
-            priority.unshift(Creep.action.storing);
         }
         for(var iAction = 0; iAction < priority.length; iAction++) {
             var action = priority[iAction];
