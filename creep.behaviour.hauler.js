1d4944a7d5dbb3529a7dd6753cbafc3b240983e6
cyberblast <git@cyberblast.org>
2016-09-10 00:13:40 +0200

    Handle container decay rate
    #129
    Thx @BringerOD

--- creep.behaviour.hauler.js
+++ creep.behaviour.hauler.js
@@ -39,6 +39,9 @@ module.exports = {
         if( _.sum(creep.carry) > creep.carry.energy ) {
             priority.unshift(Creep.action.storing);
         }
+        if( creep.room.urgentRepairableSites.length > 0 ) {
+            priority.unshift(Creep.action.fueling); 
+        }
         for(var iAction = 0; iAction < priority.length; iAction++) {
             var action = priority[iAction];
             if(action.isValidAction(creep) && 
