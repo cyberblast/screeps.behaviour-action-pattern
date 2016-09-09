137ad145d326b32ac57faebdacfd70c1509e6ce0
cyberblast <git@cyberblast.org>
2016-09-09 08:54:14 +0200

    Get rid of idle flag
    #99

--- creep.action.idle.js
+++ creep.action.idle.js
@@ -1,23 +1,21 @@
 var action = new Creep.Action('idle');
+action.targetRange = 0;
 action.isValidAction = function(creep){ return true; };
 action.isAddableAction = function(creep){ return true; };
 action.isAddableTarget = function(target){ return true; }; 
 action.newTarget = function(creep){
-    var flag = FlagDir.find(FLAG_COLOR.idle, creep.pos, true);
-    if( flag ) return flag;
+    /*var flag = FlagDir.find(FLAG_COLOR.idle, creep.pos, true);
+    if( flag ) return flag;*/ // get rid of idle flag
     if( creep.data.homeRoom && creep.pos.roomName != creep.data.homeRoom){
         // go to home room
         var exitDir = creep.room.findExitTo(creep.data.homeRoom);
         var exit = creep.pos.findClosestByRange(exitDir);
         return exit;
     }
+    if( creep.room.storage ) return creep.room.storage;
     return creep.room.controller;
 };
-action.step = function(creep) {
-    if(CHATTY) creep.say(this.name, SAY_PUBLIC);
-    if(creep.target && creep.pos != creep.target.pos) {
-        creep.moveTo(creep.target, {reusePath: 10});
-    } 
+action.work = function(creep){
     creep.data.actionName = null;
     creep.data.targetId = null;
 };
