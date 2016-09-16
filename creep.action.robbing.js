f4132966f058131d303178ee2cfa8ee439d19541
cyberblast <git@cyberblast.org>
2016-09-16 14:23:49 +0200

    Refactoring robbing

--- creep.action.robbing.js
+++ creep.action.robbing.js
@@ -13,17 +13,18 @@ action.isValidTarget = function(target){
     return ( (target != null)  );
 };  
 action.newTarget = function(creep){
-    let t = creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES, {
-        filter: function(o){
-            return o.energy >=  (creep.carryCapacity - _.sum(creep.carry)); 
-            // TODO: check for ANY resource
-            // TODO: o.energy correct? may be o.store.energy
-            // TODO: testing
+    let target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+        filter: function(structure){
+            return structure.store && !structure.my &&  _.sum(structure.store) > 0; 
         }
     });
-    return t;
+    return target;
 };
-action.work = function(creep){
-    return creep.withdraw(creep.target, RESOURCE_ENERGY);
+action.work = function(creep){    
+    for( var type in creep.target.store ){
+        if( creep.target.store[type] > 0 )
+            return creep.withdraw(creep.target, type);
+    }
+    return ERR_NOT_ENOUGH_RESOURCES;
 };
 module.exports = action;
