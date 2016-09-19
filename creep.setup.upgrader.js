656eebbcdbea31d8a60bb1a588267d7050f0b124
cyberblast <git@cyberblast.org>
2016-09-19 06:52:26 +0200

    Update creep.setup.upgrader.js

--- creep.setup.upgrader.js
+++ creep.setup.upgrader.js
@@ -15,9 +15,9 @@ setup.minEnergyAvailable = function(spawn){
     return 0.5;
 };
 setup.maxCount = function(spawn){
-    if (!spawn.room.storage || spawn.room.situation.invasion || spawn.room.conserveForDefense || spawn.room.containerController.length == 0 ) 
+    if (spawn.room.situation.invasion || spawn.room.conserveForDefense || spawn.room.containerController.length == 0 ) 
         return 0;
-    return 1 + Math.floor(spawn.room.storage.store.energy / MAX_STORAGE_ENERGY);
+    return spawn.room.storage ? 1 + Math.floor(spawn.room.storage.store.energy / MAX_STORAGE_ENERGY) : 1;
 };
 setup.maxWeight = function(spawn){
     return null;
