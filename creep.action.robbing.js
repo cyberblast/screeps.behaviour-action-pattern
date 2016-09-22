2336dbb356d8f566c82d8d1aa7fda547f36a497b
cyberblast <git@cyberblast.org>
2016-09-22 21:16:06 +0200

    Fixing robbing

--- creep.action.robbing.js
+++ creep.action.robbing.js
@@ -1,30 +1,33 @@
 var action = new Creep.Action('robbing');
 action.maxPerTarget = 2;
-action.isAddableAction = function (target) {
-    return !target.my;
-}
+/*action.isAddableTarget = function (target) {
+    return true;//!target.my;
+}*/
 action.isValidAction = function(creep){
-    return (_.sum(creep.carry) < creep.carryCapacity &&
-    FlagDir.find(FLAG_COLOR.invade.robbing, creep.pos) != null
-        // && (creep.room.energyAvailable < creep.room.energyCapacityAvailable || creep.room.towerFreeCapacity > 500 )
-    );
+    return ( _.sum(creep.carry) < creep.carryCapacity && (FlagDir.find(FLAG_COLOR.invade.robbing, creep.pos, true) != null) );
 };
 action.isValidTarget = function(target){
-    return ( (target != null)  );
+    return ( target.store && _.sum(target.store) > 0 ) || ( target.energy && target.energy > 0 );
 };  
 action.newTarget = function(creep){
+    let that = this;
     let target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
         filter: function(structure){
-            return structure.store && !structure.my &&  _.sum(structure.store) > 0; 
+            return that.isValidTarget(structure);// && that.isAddableTarget(structure); 
         }
     });
     return target;
 };
-action.work = function(creep){    
-    for( var type in creep.target.store ){
-        if( creep.target.store[type] > 0 )
-            return creep.withdraw(creep.target, type);
+action.work = function(creep){
+    let ret = OK;
+    if( creep.target.store ) {
+        for( var type in creep.target.store ){
+            if( creep.target.store[type] > 0  )
+                ret = creep.withdraw(creep.target, type);
+        }
+    } else if ( creep.target.energy ) {
+        ret = creep.withdraw(creep.target, 'energy');
     }
-    return ERR_NOT_ENOUGH_RESOURCES;
+    return ret;
 };
 module.exports = action;
--- room.js
+++ room.js
@@ -104,7 +104,7 @@ var mod = {
                 get: function() {
                     if( _.isUndefined(this._constructionSites) ) { 
                         this._constructionSites = this.find(FIND_MY_CONSTRUCTION_SITES); 
-                        let siteOrder = [STRUCTURE_EXTENSION,STRUCTURE_STORAGE,STRUCTURE_TOWER,STRUCTURE_ROAD];
+                        let siteOrder = [STRUCTURE_SPAWN,STRUCTURE_EXTENSION,STRUCTURE_STORAGE,STRUCTURE_TOWER,STRUCTURE_ROAD];
                         let getOrder = site => {let o = siteOrder.indexOf(site); return o < 0 ? 100 : o;};
                         this._constructionSites.sort( (a, b) => {return getOrder(a.structureType) - getOrder(b.structureType);} );
                     }
