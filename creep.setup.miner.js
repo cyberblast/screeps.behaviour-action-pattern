797122fab5d372f300ea9ac5978fbaec150684e3
cyberblast <git@cyberblast.org>
2016-09-26 22:48:37 +0200

    dont spawn micro miners
    then better to rely on workers.

--- creep.setup.miner.js
+++ creep.setup.miner.js
@@ -1,10 +1,10 @@
 var setup = new Creep.Setup('miner');
 setup.minControllerLevel = 3;
-setup.multiBody = [WORK];
-setup.fixedBody = [WORK, CARRY, MOVE];
-setup.minAbsEnergyAvailable = 200;
-setup.maxMulti = function(room){ return 4; };
-setup.sortedParts = false;
+setup.multiBody = [WORK,MOVE];
+setup.fixedBody = [WORK, WORK, WORK, WORK, CARRY, MOVE];
+setup.minAbsEnergyAvailable = 500;
+setup.maxMulti = function(room){ return 1; };
+setup.sortedParts = true;
 setup.minEnergyAvailable = function(spawn){
     return 0.3;
 };
