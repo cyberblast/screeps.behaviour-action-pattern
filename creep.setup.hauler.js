27520e570dcdd1747e845f02dea1945c40b23ccc
cyberblast <git@cyberblast.org>
2016-11-03 15:02:13 +0100

    Handle energy distribution bottleneck
    Additional hauler when lots of energy in storage but none in container of the controller

--- creep.setup.hauler.js
+++ creep.setup.hauler.js
@@ -14,7 +14,7 @@ setup.maxCount = function(room){
     let count = 0;
     if(room.population && room.population.typeCount['miner'] > 0) {
         count += Creep.setup.upgrader.maxCount(room);
-        if( room.links.length < 3) count++;
+        if( room.links.length < 3 || (room.storage && room.storage.energy > MAX_STORAGE_ENERGY && room.containerController && _.sum(room.containerController, 'store.energy') == 0 ) count++;
     }
     return count; 
 };
