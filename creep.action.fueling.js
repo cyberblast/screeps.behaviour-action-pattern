23e46181d1d6c7bf01b4be84e531520bc0224bb7
cyberblast <git@cyberblast.org>
2016-11-05 10:42:21 +0100

    new evade & fueling symbols

--- creep.action.fueling.js
+++ creep.action.fueling.js
@@ -21,6 +21,6 @@ action.work = function(creep){
     return response;
 };
 action.onAssignment = function(creep, target) {
-    if( SAY_ASSIGNMENT ) creep.say(String.fromCharCode(9981), SAY_PUBLIC); 
+    if( SAY_ASSIGNMENT ) creep.say('\u{26FD}\u{FE0E}', SAY_PUBLIC);
 };
 module.exports = action;
