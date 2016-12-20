0b4d27dc29d2c7808701e180b781d61d14d095bb
cyberblast <git@cyberblast.org>
2016-12-20 17:40:40 +0100

    updating override system

--- main.js
+++ main.js
@@ -4,16 +4,26 @@ module.exports.loop = function () {
     global.getPath = (modName, reevaluate = false) => {
         if (Memory.modules === undefined) 
             Memory.modules = {};
+        if (Memory.modules.viral === undefined) 
+            Memory.modules.viral = {};
         if( reevaluate || !Memory.modules[modName] ){
             let path = './custom.' + modName;
             try {
-                var a = require(path);
+                let a = require(path);
             }
             catch (e) {
                 path = './' + modName
             }
             finally {
                 Memory.modules[modName] = path;
+            }
+            path = './viral.' + modName;
+            try {
+                let viralOverride = require(path);
+                if( viralOverride ) Memory.modules.viral[modName] = true;
+            }
+            catch (e) {
+                delete Memory.modules.viral[modName];
             }
         }
         return Memory.modules[modName];
@@ -42,29 +52,51 @@ module.exports.loop = function () {
                 console.log(`Error loading module "${modName}"!<br/>${e.toString()}`);
             }
         }
+        if( mod && Memory.modules.viral[modName] ) {
+            let viralOverride;
+            try {
+                viralOverride = require('./viral.' + modName);
+            }
+            catch (e) {
+                viralOverride = null;
+                if( e.message && e.message.indexOf('Unknown module') > -1 ){
+                    console.log(`Viral override for "${modName}" not found!`);
+                } else if(mod == null) {
+                    console.log(`Error loading viral override "${modName}"!<br/>${e.toString()}`);
+                }
+            }
+            if( viralOverride ) _.assign(mod, viralOverride);
+            else delete Memory.modules.viral[modName];
+        }
         return mod;
     };
 
+    // initialize global & parameters
     let params = load('parameter');
     let glob = load('global');
     glob.init(params);
-    if (Memory.modules === undefined) loadPaths();
+
+    // Extend Server Objects
     Extensions.extend();  
     Creep.extend();  
     Room.extend();  
     Spawn.extend();
     FlagDir.extend();
 
-    FlagDir.loop();
+    // Analyze environment
     Population.loop();
+    FlagDir.loop();
     let roomLoop = room => {
         room.loop();
         Tower.loop(room);
     };
     _.forEach(Game.rooms, roomLoop);
+
+    // Execution
     Creep.loop();   
     Spawn.loop(); 
 
+    // Evaluation
     if( Memory.statistics && Memory.statistics.tick && Memory.statistics.tick + TIME_REPORT <= Game.time )
         load('statistics').loop();
     processReports();
