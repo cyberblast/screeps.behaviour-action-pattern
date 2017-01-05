cc2bee2a1693beeae4804eff4cdc9b4ad6e17fa0
cyberblast <git@cyberblast.org>
2017-01-05 23:06:20 +0100

    replacing this with setup (#409)
    * replacing this with setup
    
    * fixing copy paste error, removed 'creep.'
    
    * adding missing param
    
    * use room instead of creep.room

--- creep.setup.worker.js
+++ creep.setup.worker.js
@@ -3,7 +3,7 @@ setup.maxWorker = room => {
     // no hauler and no miner => 1
     // if there is a miner it should be no problem to spawn a hauler, and vice versa. 
     // if none of them are present spawn a worker first
-    if( !this.hasMinerOrHauler(creep.room))
+    if( !setup.hasMinerOrHauler(room))
         return 1;
     // constructionsites present & no strorage or storage > min
     if( room.constructionSites.length > 0 && !room.storage || room.storage.store.energy > MIN_STORAGE_ENERGY[room.controller.level]) 
@@ -14,9 +14,9 @@ setup.maxWorker = room => {
     return 0;
 };
 // validates if there is a miner or a hauler present
-setup.hasMinerOrHauler = room => ( creep.room.population &&
-    ((creep.room.population.typeCount['hauler'] && creep.room.population.typeCount['hauler'] > 0) ||
-    (creep.room.population.typeCount['miner'] && creep.room.population.typeCount['miner'] > 0 )));
+setup.hasMinerOrHauler = room => ( room.population &&
+    ((room.population.typeCount['hauler'] && room.population.typeCount['hauler'] > 0) ||
+    (room.population.typeCount['miner'] && room.population.typeCount['miner'] > 0 )));
 setup.RCL = {
     1: {
         fixedBody: [],
@@ -40,7 +40,7 @@ setup.RCL = {
         fixedBody: [],
         multiBody: [CARRY, WORK, MOVE],
         minAbsEnergyAvailable: 200,
-        minEnergyAvailable: room => this.hasMinerOrHauler(room) ? 0.3 : 0,
+        minEnergyAvailable: room => setup.hasMinerOrHauler(room) ? 0.3 : 0,
         maxMulti: 8,
         maxCount: room => ( room.situation.invasion ) ? 1 : 4,
         maxWeight: 9600
@@ -49,45 +49,45 @@ setup.RCL = {
         fixedBody: [],
         multiBody: [CARRY, WORK, MOVE],
         minAbsEnergyAvailable: 200,
-        minEnergyAvailable: room => this.hasMinerOrHauler(room) ? 0.3 : 0,
+        minEnergyAvailable: room => setup.hasMinerOrHauler(room) ? 0.3 : 0,
         maxMulti: 8,
-        maxCount: (room) => ( room.situation.invasion || room.storage ) ? this.maxWorker(room) : 2,
+        maxCount: (room) => ( room.situation.invasion || room.storage ) ? setup.maxWorker(room) : 2,
         maxWeight: 4800
     },
     5: {
         fixedBody: [],
         multiBody: [CARRY, WORK, MOVE],
         minAbsEnergyAvailable: 400,
-        minEnergyAvailable: room => this.hasMinerOrHauler(room) ? 0.3 : 0,
+        minEnergyAvailable: room => setup.hasMinerOrHauler(room) ? 0.3 : 0,
         maxMulti: 8,
-        maxCount: this.maxWorker(room),
+        maxCount: room => setup.maxWorker(room),
         maxWeight: 2000
     },
     6: {
         fixedBody: [],
         multiBody: [CARRY, WORK, MOVE],
         minAbsEnergyAvailable: 600,
-        minEnergyAvailable: room => this.hasMinerOrHauler(room) ? 0.3 : 0,
+        minEnergyAvailable: room => setup.hasMinerOrHauler(room) ? 0.3 : 0,
         maxMulti: 8,
-        maxCount: this.maxWorker(room),
+        maxCount: room => setup.maxWorker(room),
         maxWeight: 2000
     },
     7: {
         fixedBody: [],
         multiBody: [CARRY, WORK, MOVE],
         minAbsEnergyAvailable: 800,
-        minEnergyAvailable: room => this.hasMinerOrHauler(room) ? 0.2 : 0,
+        minEnergyAvailable: room => setup.hasMinerOrHauler(room) ? 0.2 : 0,
         maxMulti: 10,
-        maxCount: this.maxWorker(room),
+        maxCount: room => setup.maxWorker(room),
         maxWeight: 2000
     },
     8: {
         fixedBody: [],
         multiBody: [CARRY, WORK, MOVE],
         minAbsEnergyAvailable: 800,
-        minEnergyAvailable: room => this.hasMinerOrHauler(room) ? 0.1 : 0,
+        minEnergyAvailable: room => setup.hasMinerOrHauler(room) ? 0.1 : 0,
         maxMulti: room => (( !room.storage || room.storage.energy > MAX_STORAGE_ENERGY[8] ) ? 16 : 10),
-        maxCount: this.maxWorker(room),
+        maxCount: room => setup.maxWorker(room),
         maxWeight: 3200
     }
 };
