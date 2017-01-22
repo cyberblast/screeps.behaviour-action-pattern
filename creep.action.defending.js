ce69789150d3d201dea3426a82e2f24a53ff206b
cyberblast <git@cyberblast.org>
2017-01-22 11:07:43 +0100

    fixing no direction (#599)

--- creep.action.defending.js
+++ creep.action.defending.js
@@ -25,29 +25,29 @@ action.step = function(creep){
 };
 action.run = {
     ranger: function(creep) {
-        var range = creep.pos.getRangeTo(creep.target);
+        let range = creep.pos.getRangeTo(creep.target);
         if( !creep.flee ){
             if( range > 3 ){
-                var path = creep.room.findPath(creep.pos, creep.target.pos, {ignoreCreeps: false});
+                let path = creep.room.findPath(creep.pos, creep.target.pos, {ignoreCreeps: false});
                 if( path && path.length > 0 ) {
-                    var isRampart = COMBAT_CREEPS_RESPECT_RAMPARTS && _.some( creep.room.lookForAt(LOOK_STRUCTURES, path[0].x, path[0].y), {'structureType': STRUCTURE_RAMPART });
+                    let isRampart = COMBAT_CREEPS_RESPECT_RAMPARTS && _.some( creep.room.lookForAt(LOOK_STRUCTURES, path[0].x, path[0].y), {'structureType': STRUCTURE_RAMPART });
                     if(!isRampart){
                         creep.move(path[0].direction);
                     }
                 } else {
                     // no path -> try to move by direction
-                    var direction = creep.pos.getDirectionTo(creep.target);
-                    creep.move(direction);
+                    let direction = creep.pos.getDirectionTo(creep.target);
+                    if( direction ) creep.move(direction);
                 }
             }
             if( range < 3 ){
-                var direction = creep.target.pos.getDirectionTo(creep);
-                creep.move(direction);
+                let direction = creep.target.pos.getDirectionTo(creep);
+                if( direction ) creep.move(direction);
             }
         }
 
         // attack ranged
-        var targets = creep.pos.findInRange(creep.room.hostiles, 3);
+        let targets = creep.pos.findInRange(creep.room.hostiles, 3);
         if(targets.length > 2) { // TODO: precalc damage dealt
             if(CHATTY) creep.say('MassAttack');
             creep.attackingRanged = creep.rangedMassAttack() == OK;
@@ -63,7 +63,7 @@ action.run = {
     },
     melee: function(creep) {
         if( !creep.flee ){
-            var path = creep.room.findPath(creep.pos, creep.target.pos);
+            let path = creep.room.findPath(creep.pos, creep.target.pos);
             // not standing in rampart or next step is rampart as well
             if( path && path.length > 0 && (
                 !COMBAT_CREEPS_RESPECT_RAMPARTS ||
