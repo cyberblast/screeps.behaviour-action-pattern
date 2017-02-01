94b14af4fcffbce13623b34559cd53d58cf42d2c
Walter Hardeman <whardeman@qualbe.com>
2017-01-31 21:41:47 -0600

    Clean up and simplify to improve readability

--- creep.action.reallocating.js
+++ creep.action.reallocating.js
@@ -20,11 +20,13 @@ action.isValidEnergyToStorage = function(creep){
     return creep.room.terminal.store.energy + creep.carry.energy > TERMINAL_ENERGY * 1.05;
 };
 action.isValidMineralToStorage = function(creep){
-    let mineral = creep.room.terminal.store[creep.room.mineralType] || 0;
-    return creep.room.terminal.sum > mineral + creep.room.terminal.store.energy;
+    let terminal = creep.room.terminal;
+    let mineral = terminal.store[creep.room.mineralType] || 0;
+    return terminal.sum > mineral + terminal.store.energy;
 };
 action.isAddableAction = function(creep){
-    return creep.sum == 0 &&(!creep.room.population || !creep.room.population.actionCount[this.name] || creep.room.population.actionCount[this.name] < this.maxPerAction);
+    let pop = creep.room.population;
+    return creep.sum == 0 &&(!pop || !pop.actionCount[this.name] || pop.actionCount[this.name] < this.maxPerAction);
 };
 action.isValidTarget = function(target){
     return true;
@@ -33,48 +35,53 @@ action.isAddableTarget = function(target){
     return true;
 };
 action.newTarget = function(creep){
-    if( (creep.sum == 0) == (creep.room.terminal.store.energy > TERMINAL_ENERGY*1.05 || this.isValidMineralToStorage(creep)) ) return creep.room.terminal;
+    let terminal = creep.room.terminal;
+    if( (creep.sum == 0) == (terminal.store.energy > TERMINAL_ENERGY*1.05 || this.isValidMineralToStorage(creep)) ) return terminal;
     else return creep.room.storage;
 };
 action.work = function(creep){
+    let target = creep.target;
+    let room = creep.room;
+    let storage = room.storage;
+    let terminal = room.terminal;
     var workResult = null;
-    if( creep.sum == 0 && creep.target.structureType == STRUCTURE_STORAGE ){
+    if( creep.sum == 0 && target.structureType == STRUCTURE_STORAGE ){
         // load: storage => terminal
-        workResult = creep.withdraw(creep.target, creep.room.mineralType);
-        this.assign(creep, creep.room.terminal);
+        workResult = creep.withdraw(target, room.mineralType);
+        this.assign(creep, terminal);
         delete creep.data.path;
-    } else if( creep.sum == 0 && creep.target.structureType == STRUCTURE_TERMINAL ) {
+    } else if( creep.sum == 0 && target.structureType == STRUCTURE_TERMINAL ) {
         // load: terminal => storage
         if( this.isValidEnergyToStorage(creep) )
-            workResult = creep.withdraw(creep.target, RESOURCE_ENERGY);
+            workResult = creep.withdraw(target, RESOURCE_ENERGY);
         else if( this.isValidMineralToStorage(creep) ){
             // TODO: get minerals != room mineral
             let withdraw = r => {
-                if( r != RESOURCE_ENERGY && r != creep.room.mineralType && creep.target.store[r] > 0 )
-                    workResult = creep.withdraw(creep.target, r);
+                if( r != RESOURCE_ENERGY && r != room.mineralType && target.store[r] > 0 )
+                    workResult = creep.withdraw(target, r);
             };
-            _.forEach(Object.keys(creep.target.store), withdraw);
+            _.forEach(Object.keys(target.store), withdraw);
         }
-        this.assign(creep, creep.room.storage);
+        this.assign(creep, storage);
         delete creep.data.path;
-    } else if( creep.target.structureType == STRUCTURE_TERMINAL ) {
+    } else if( target.structureType == STRUCTURE_TERMINAL ) {
         // deliver: storage => terminal
-        workResult = creep.transfer(creep.target, creep.room.mineralType);
+        workResult = creep.transfer(target, room.mineralType);
         // unregister action
         delete creep.data.actionName;
         delete creep.data.targetId;
         creep.action = null;
         creep.target = null;
         delete creep.data.path;
-    } else if( creep.target.structureType == STRUCTURE_STORAGE ) {
+    } else if( target.structureType == STRUCTURE_STORAGE ) {
         // deliver: terminal => storage
         for(let resourceType in creep.carry) {
             if( creep.carry[resourceType] > 0 ){
-                workResult = creep.transfer(creep.target, resourceType);
+                workResult = creep.transfer(target, resourceType);
                 if( workResult != OK ) break;
             }
         }
-        //workResult = creep.transfer(creep.target, RESOURCE_ENERGY);
+        //workResult = creep.transfer(target, RESOURCE_ENERGY);
         // unregister action
         if( creep.sum == creep.carry.energy ){
             delete creep.data.actionName;
