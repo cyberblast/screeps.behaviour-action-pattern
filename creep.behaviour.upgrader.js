ba39339b8586827a1ef9f0ffab5aa27934ff7ad2
Chobobobo <chobobobo@users.noreply.github.com>
2017-02-17 01:55:39 +0800

    Forget bad spot selection, select any valid spot in range to *any* valid structure, rather than *all* valid structures, start upgrading as soon as it is in range.

--- creep.behaviour.upgrader.js
+++ creep.behaviour.upgrader.js
@@ -1,11 +1,21 @@
 let mod = {};
 module.exports = mod;
 mod.name = 'upgrader';
+let invalidCreep = c => ['miner', 'upgrader'].includes(c.data.creepType) && c.data.determinatedSpot &&
+    (c.data.ttl > c.data.spawningTime || c.data.ttl > c.data.predictedRenewal);
 mod.approach = function(creep){
     let targetPos = new RoomPosition(creep.data.determinatedSpot.x, creep.data.determinatedSpot.y, creep.pos.roomName);
     let range = creep.pos.getRangeTo(targetPos);
-    if( range > 0 )
+    if( range > 0 ) {
+        if (range === 1) {
+            const creeps = targetPos.lookFor(LOOK_CREEPS);
+            if (creeps.length && _.some(creeps, invalidCreep)) {
+                // forget spots that have been improperly selected/unable to move to
+                delete creep.data.determinatedSpot;
+            }
+        }
         creep.drive( targetPos, 0, 0, range );
+    }
     return range;
 };
 mod.run = function(creep) {
@@ -15,63 +25,45 @@ mod.run = function(creep) {
     }
     if( !creep.action ) Population.registerAction(creep, Creep.action.upgrading, creep.room.controller);
     if( !creep.data.determinatedSpot ) {
-        let args = {
-            spots: [{
-                pos: creep.room.controller.pos,
-                range: 3
-            }],
-            checkWalkable: true,
-            where: null,
-            roomName: creep.pos.roomName
-        }
-        let addSpot = s => args.spots.push({
-            pos: s.pos,
-            range: 1
-        });
-        if( creep.room.structures.container.controller ){
-            creep.room.structures.container.controller.forEach(addSpot);
-        }
-        if( creep.room.structures.links.controller ){
-            creep.room.structures.links.controller.forEach(addSpot);
-        }
-        // dont take already taken
-        let taken = [];
-        let findInvalid = entry => {
-            if( entry.roomName == args.roomName && ['miner', 'upgrader'].includes(entry.creepType) && entry.determinatedSpot && entry.ttl > entry.spawningTime)
-                taken.push(entry.determinatedSpot)
+        let determineSpots = (ignoreSources=false) => {
+            let getSpots = s => {
+                let args = {
+                    spots: [{
+                        pos: creep.room.controller.pos,
+                        range: 3
+                    },
+                    {
+                        pos: s.pos,
+                        range: 1
+                    }],
+                    checkWalkable: true,
+                    where: pos => !_.some(pos.lookFor(LOOK_CREEPS), invalidCreep) && (ignoreSources || pos.findInRange(creep.room.sources, 1).length === 0),
+                    roomName: creep.pos.roomName
+                };
+                spots = spots.concat(Room.fieldsInRange(args));
+            };
+            if (creep.room.structures.container.controller) creep.room.structures.container.controller.forEach(getSpots);
+            if (creep.room.structures.links.controller) creep.room.structures.links.controller.forEach(getSpots);
         };
-        _.forEach(Memory.population, findInvalid);
-        // dont take miner spots
-        let invalid = taken.slice(0);
-        let sourcesInRange = creep.room.controller.pos.findInRange(creep.room.sources, 4);
-        let addAdjacent = source => source.pos.adjacent.forEach(pos => invalid.push({x:pos.x,y:pos.y}))
-        sourcesInRange.forEach(addAdjacent);
-
-        args.where = pos => { return !_.some(invalid,{x:pos.x,y:pos.y}); };
-        let spots = Room.fieldsInRange(args);
-        if( spots.length == 0 ){ 
-            // no position found. allow pos near sources
-            args.where = pos => { return !_.some(taken,{x:pos.x,y:pos.y}); };
-            spots = Room.fieldsInRange(args);
-        }
-        if( spots.length == 0 ){ 
-            // no position found. allow any
-            delete args.where;
-            spots = Room.fieldsInRange(args);
-        }
+        let spots = determineSpots();
         if( spots.length > 0 ){
+            // allow spots near sources
+            spots = determineSpots(true);
+        }
+        if (spots.length > 0) {
+            // prefer off roads
             let spot = creep.pos.findClosestByPath(spots, {filter: pos => {
                 return !_.some(
                     creep.room.lookForAt(LOOK_STRUCTURES, pos),
                     {'structureType': STRUCTURE_ROAD }
                 );
-            }})
+            }});
             if( !spot ) spot = creep.pos.findClosestByPath(spots) || spots[0];
             if( spot ) {
                 creep.data.determinatedSpot = {
                     x: spot.x,
                     y: spot.y
-                }
+                };
                 let spawn = Game.spawns[creep.data.motherSpawn];
                 if( spawn ) {
                     let path = spot.findPathTo(spawn, {ignoreCreeps: true});
@@ -85,7 +77,7 @@ mod.run = function(creep) {
     if( creep.data.determinatedSpot ) {
         if(CHATTY) creep.say('upgrading', SAY_PUBLIC);
         let range = this.approach(creep);
-        if( range == 0 ){
+        if( creep.room.controller && creep.pos.getRangeTo(creep.room.controller) <= 3){
             let carryThreshold = (creep.data.body&&creep.data.body.work ? creep.data.body.work : (creep.carryCapacity/2));
             if( creep.carry.energy <= carryThreshold ){
                 let store = creep.room.structures.links.controller.find(l => l.energy > 0);
