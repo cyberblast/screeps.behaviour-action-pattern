7ecfb301f425b98cad336f404d7d7057ab5995de
Logxen <logxen@hotmail.com>
2017-02-17 14:55:46 -0800

    treat unbuilt roads as if they were complete for traveler

--- parameter.js
+++ parameter.js
@@ -7,6 +7,7 @@ let mod = {
     DEBUG: true, // gimme some more details, use false not undefined to unset
     TRACE: false, // use Memory.debugTrace for low-level information
     TRAVELER_THRESHOLD: 50, // CPU Usage before warning about pathing cost
+    USE_UNBUILT_ROADS: true,
     GRAFANA: false, // track for Grafana data
     GRAFANA_INTERVAL: 3, // loops between Grafana tracking - No lower than 3.
     CENSUS_ANNOUNCEMENTS: true, // log birth and death
