22f33de60e41b3c7525276f1c8d312b1065f31ff
Logxen <logxen@hotmail.com>
2017-02-20 06:26:10 -0800

    fixed string to object comparison and added log

--- creep.js
+++ creep.js
@@ -42,7 +42,7 @@ mod.extend = function(){
             if(!behaviour && this.data && this.data.creepType) {
                 behaviour = Creep.behaviour[this.data.creepType];
             }
-            if ( Game.cpu.bucket < CRITICAL_BUCKET_LEVEL && !CRITICAL_ROLES.includes(behaviour) ) {
+            if ( Game.cpu.bucket < CRITICAL_BUCKET_LEVEL && !CRITICAL_ROLES.includes(creep.data.creepType) ) {
                 return;
             }
             this.repairNearby();
@@ -447,6 +447,7 @@ mod.extend = function(){
     Creep.prototype.customStrategy = function(actionName, behaviourName, taskName) {};
 };
 mod.execute = function(){
+    if ( DEBUG && Game.cpu.bucket < CRITICAL_BUCKET_LEVEL ) logSystem('system',`${Game.time}: CPU Bucket level is critical (${Game.cpu.bucket}). Skipping non critical creep roles.`);
     let run = creep => creep.run();
     _.forEach(Game.creeps, run);
 };
