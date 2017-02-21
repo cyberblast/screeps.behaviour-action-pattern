88516f77fc3b11da160681f8536e8ce7c7b8394c
Karl the Pagan <karlthepagan@gmail.com>
2017-02-20 16:16:19 -0800

    avoiding action WIP

---
+++ creep.action.avoiding.js
@@ -0,0 +1,60 @@
+let action = new Creep.Action('avoiding');
+module.exports = action;
+action.targetRange = 0;
+action.reachedRange = 0;
+action.isActiveLair = function(target) {
+    return !(target.ticksToSpawn > 12);
+};
+action.isValidAction = function(creep){
+    return creep.room.situation.invasion || Room.isSKRoom(creep.pos.roomName);
+};
+this.isValidTarget = function(target, creep){
+    return Task.reputation.hostileOwner(target) && action.isActiveLair(target);
+};
+action.newTarget = function(creep) {
+    if (Room.isSKRoom(creep.pos.roomName)) {
+        const target = _.first(creep.room.find(FIND_STRUCTURES, {filter: function (t) {
+            return action.isActiveLair(t) && creep.pos.getRangeTo(t.pos) < 15;
+        }}));
+
+        if (target) {
+            return target;
+        }
+    }
+
+    if (creep.room.situation.invasion) {
+        const target = _.chain(creep.room.hostiles).sortBy(function(c) {
+            return creep.pos.getRangeTo(c);
+        }).first().value();
+
+        if (target) {
+            return target;
+        }
+    }
+};
+action.work = function(creep) {
+    if (!creep.data.determinatedSpot) {
+        let targets = _.chain(creep.room.hostiles).filter(function(c) {
+            return creep.pos.getRangeTo(c) < 15;
+        });
+
+        if (action.isActiveLair(creep.target) ) {
+            targets = targets.concat(creep.target);
+        }
+
+        targets = targets.map(function(t) {
+            _.create(t,{
+                // move 4 away from enemies
+                // move 6 away from lairs
+                range: t.ticksToSpawn !== undefined ? 4 : 6,
+            });
+        }).value();
+
+        // TODO pathfinder to pick a spot
+    }
+
+    // TODO move to the spot
+};
+action.onAssignment = function(creep, target) {
+    if( SAY_ASSIGNMENT ) creep.say(String.fromCharCode(10532), SAY_PUBLIC);
+};
