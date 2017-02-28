6e184fec9d41cb63601c76ea2b4aba2a24501140
Dan (Asku) <askewholdings@gmail.com>
2017-02-28 11:20:22 +0000

    Corrected spacing

--- room.js
+++ room.js
@@ -1047,13 +1047,13 @@ mod.extend = function(){
         else this.roadConstructionTrace[key]++;
     };
     Room.prototype.checkPowerBank = function() {
-        if (!this.powerBank) return; // no power bank in room
-    	//power > 2500, and ticksToDecay > 4500
-    	if(this.powerBank && this.powerBank.power > 2500 && this.powerBank.ticksToDecay > 4500){
-    	    // Place a flag
-    	    this.createFlag(this.powerBank.pos, null, FLAG_COLOR.invade.powerMining.color, FLAG_COLOR.invade.powerMining.secondaryColor);
-    	}
-   	};
+    if (!this.powerBank) return; // no power bank in room
+        //power > 2500, and ticksToDecay > 4500
+        if(this.powerBank && this.powerBank.power > 2500 && this.powerBank.ticksToDecay > 4500){
+            // Place a flag
+            this.createFlag(this.powerBank.pos, null, FLAG_COLOR.invade.powerMining.color, FLAG_COLOR.invade.powerMining.secondaryColor);
+        }
+    };
     Room.prototype.saveTowers = function(){
         let towers = this.find(FIND_MY_STRUCTURES, {
             filter: {structureType: STRUCTURE_TOWER}
