3210b41254e3678ad53908d6511302512fa694ab
Liam <Spedwards@users.noreply.github.com>
2017-03-05 03:37:32 +1100

    Fix Visuals Undefined Error (#914)
    Resolves #913

--- main.js
+++ main.js
@@ -288,7 +288,7 @@ module.exports.loop = function () {
     // custom cleanup
     if( global.mainInjection.cleanup ) global.mainInjection.cleanup();
 
-    if ( ROOM_VISUALS && !Memory.CPU_CRITICAL ) Visuals.run(); // At end to correctly display used CPU.
+    if ( ROOM_VISUALS && !Memory.CPU_CRITICAL && Visuals ) Visuals.run(); // At end to correctly display used CPU.
 
     if ( GRAFANA && Game.time % GRAFANA_INTERVAL === 0 ) Grafana.run();
 
