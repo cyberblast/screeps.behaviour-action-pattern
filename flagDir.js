bd12e490bef082347dbde028c346aeae3ec80c63
Liam <Spedwards@users.noreply.github.com>
2017-04-01 16:44:53 +1100

    Feature/easily create flags (#1098)
    * reintegrate ocs.public before branching to canary
    
    * Easily create flags

--- flagDir.js
+++ flagDir.js
@@ -162,6 +162,36 @@ mod.extend = function(){
             return Flag.compare(this, flag);
         },
     });
+    
+    Object.defineProperty(RoomPosition.prototype, 'newFlag', {
+        configurable: true,
+        /**
+         * Create a new flag at this position
+         * @param {Object|string} flagColour - An object with color and secondaryColor properties, or a string path for a FLAG_COLOR
+         * @param {string} [name] - Optional name for the flag
+         * @returns {string|Number} The name of the flag or an error code.
+         */
+        value: function(flagColour, name) {
+            if (!flagColour) flagColour = _.get(FLAG_COLOR, flagColour); // allows you to pass through a string (e.g. 'invade.robbing')
+            if (!flagColour) return;
+            return this.createFlag(name, flagColour.color, flagColour.secondaryColor);
+        },
+    });
+    
+    Object.defineProperty(Room.prototype, 'newFlag', {
+        configurable: true,
+        /**
+         * Create a new flag
+         * @param {Object|string} flagColour - An object with color and secondaryColor properties, or a string path for a FLAG_COLOR
+         * @param {RoomPosition} [pos] - The position to place the flag. Will assume (25, 25) if left undefined
+         * @param {string} [name] - Optional name for the flag
+         * @returns {string|Number} The name of the flag or an error code.
+         */
+        value: function(flagColour, pos, name) {
+            if (!pos) pos = this.getPositionAt(25, 25);
+            return pos.newFlag(flagColour, name);
+        },
+    });
 };
 mod.flush = function(){        
     let clear = flag => delete flag.targetOf;
