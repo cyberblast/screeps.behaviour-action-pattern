c49d4c73d6f945ab7769e1aed5bf22dc089d02a4
Spedwards <improbablepuzzle@gmail.com>
2017-04-05 02:59:03 +1000

    Action.Bulldozing

--- creep.action.bulldozing.js
+++ creep.action.bulldozing.js
@@ -1,30 +1,40 @@
-const action = new Creep.Action('bulldozing');
-module.exports = action;
-action.maxPerAction = 2;
-action.maxPerTarget = 1;
-action.isValidTarget = function(target) {
-    if (!target.room.my && target.room.controller && target.room.controller.safeMode) return false;
-    return target instanceof ConstructionSite && Task.reputation.notAlly(target.owner.username);
+const action = class extends Creep.Action {
+    
+    constructor(...args) {
+        super(...args);
+        this.maxPerAction = 2;
+        this.maxPerTarget = 1;
+    }
+    
+    isValidTarget(target) {
+        if (!target.room.my && target.room.controller && target.room.controller.safeMode) return false;
+        return target instanceof ConstructionSite && Task.reputation.notAlly(target.owner.username);
+    }
+    
+    newTarget(creep) {
+        const target = _(creep.room.constructionSites)
+            .filter(this.isValidTarget)
+            .max(target => {
+                let rating;
+                if (targer.structureType === STRUCTURE_SPAWN) {
+                    rating = 20000;
+                } else {
+                    rating = 10000;
+                }
+                rating += target.progress / target.progressTotal * 10000;
+                rating -= creep.pos.getRangeTo(target);
+                return rating;
+            });
+        if (target instanceof ConstructionSite) return target;
+    }
+    
+    work(creep) {
+        return creep.move(creep.pos.getDirectionTo(creep.target));
+    }
+    
+    onAssignment(creep) {
+        if (SAY_ASSIGNMENT) creep.say(ACTION_SAY.BULLDOZING, SAY_PUBLIC);
+    }
+    
 };
-action.newTarget = function(creep) {
-    const target = _(creep.room.constructionSites)
-        .filter(action.isValidTarget)
-        .max(target => {
-            let rating;
-            if (target.structureType === STRUCTURE_SPAWN) {
-                rating = 20000;
-            } else {
-                rating = 10000;
-            }
-            rating += target.progress / target.progressTotal * 10000;
-            rating -= creep.pos.getRangeTo(target);
-            return rating;
-        });
-    if (target instanceof ConstructionSite) return target;
-};
-action.work = function(creep) {
-    return creep.move(creep.pos.getDirectionTo(creep.target));
-};
-action.onAssignment = function(creep) {
-    if (SAY_ASSIGNMENT) creep.say(ACTION_SAY.BULLDOZING, SAY_PUBLIC);
-};
+module.exports = new action('bulldozing');
