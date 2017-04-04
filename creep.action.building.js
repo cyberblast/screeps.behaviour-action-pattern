458eeae4454e2d5e356ed2f468d26fedc05136c8
Spedwards <improbablepuzzle@gmail.com>
2017-04-05 02:39:15 +1000

    Action.Building

--- creep.action.building.js
+++ creep.action.building.js
@@ -1,29 +1,40 @@
-let action = new Creep.Action('building');
-module.exports = action;
-action.maxPerTarget = 3;
-action.targetRange = 3;
-action.maxPerAction = 3;
-action.isValidAction = function(creep){
-    return ( creep.carry.energy > 0 );
+const action = class extends Creep.Action {
+    
+    constructor(...args) {
+        super(...args);
+        this.maxPerTarget = 3;
+        this.targetRange = 3;
+        this.maxPerAction = 3;
+    }
+    
+    isValidAction(creep) {
+        return creep.carry.energy > 0;
+    }
+    
+    isMineOrAlly(target) {
+        return target.my || Task.reputation.allyOwner(target);
+    }
+    
+    isValidTarget(target) {
+        return super.isValidTarget(target) && this.isMineOrAlly(target) && target.progress && target.progress < target.progressTotal;
+    }
+    
+    isAddableTarget(target) {
+        // our site?
+        return super.isAddableTarget(target) && this.isMineOrAlly(target);
+    }
+    
+    newTarget(creep) {
+        return creep.room.getBestConstructionSiteFor(creep.pos, target => this.isAddableTarget(target));
+    }
+    
+    work(creep) {
+        return creep.build(creep.target);
+    }
+    
+    onAssignment(creep) {
+        if (SAY_ASSIGNMENT) creep.say(ACTION_SAY.BUILDING, SAY_PUBLIC);
+    }
+    
 };
-action.isAddableAction = function(creep){
-    return ( !creep.room.population || !creep.room.population.actionCount[this.name] || creep.room.population.actionCount[this.name] < this.maxPerAction);
-};
-action.isValidTarget = function(target){
-    return (target != null && (target.my || Task.reputation.allyOwner(target)) && target.progress && target.progress < target.progressTotal);
-};  
-action.isAddableTarget = function(target) {
-    //  our site?
-    return target && (target.my || Task.reputation.allyOwner(target)) && (!target.targetOf || target.targetOf.length < this.maxPerTarget);
-};
-action.newTarget = function(creep){
-    var that = this;
-    var isAddable = target => that.isAddableTarget(target, creep);
-    return creep.room.getBestConstructionSiteFor(creep.pos, isAddable);
-};
-action.work = function(creep){
-    return creep.build(creep.target);
-};
-action.onAssignment = function(creep, target) {
-    if( SAY_ASSIGNMENT ) creep.say(ACTION_SAY.BUILDING, SAY_PUBLIC);
-};
+module.exports = new Action('building');
