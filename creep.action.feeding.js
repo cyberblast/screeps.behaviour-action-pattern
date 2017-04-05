08a000db790342d209618f30bdde5bb29fd8d759
Spedwards <improbablepuzzle@gmail.com>
2017-04-05 20:14:14 +1000

    Action.Feeding

--- creep.action.feeding.js
+++ creep.action.feeding.js
@@ -1,41 +1,40 @@
-let action = new Creep.Action('feeding');
-module.exports = action;
-action.maxPerTarget = 1;
-action.isValidAction = function(creep){
-    return ( creep.carry.energy > 0 && creep.room.energyAvailable < creep.room.energyCapacityAvailable );
-};
-action.isValidTarget = function(target){
-    return ( (target != null) && (target.energy != null) && (target.energy < target.energyCapacity) );
-};
-action.isAddableAction = function(creep){
-    return true;
-};
-action.isAddableTarget = function(target){
-    return ( target.my &&
-        (!target.targetOf || _.filter(target.targetOf, {'actionName':'feeding'}).length < this.maxPerTarget));
-};
-action.newTarget = function(creep){
-    if (creep.room.energyAvailable === creep.room.energyCapacityAvailable) {
-        return null;
+const action = class extends Creep.Action {
+    
+    constructor(...args) {
+        super(...args);
+        
+        this.maxPerTarget = 1;
+        this.statement = ACTION_SAY.FEEDING;
     }
-    var that = this;
-    return creep.pos.findClosestByRange(creep.room.structures.all, {
-        filter: (structure) => {
-            return ((structure.structureType == STRUCTURE_EXTENSION ||
-                structure.structureType == STRUCTURE_SPAWN )
-                && that.isValidTarget(structure) && that.isAddableTarget(structure, creep));
+    
+    isValidAction(creep) {
+        return creep.carry.energy > 0 && creep.room.energyAvailable < creep.room.energyCapacityAvailable;
+    }
+    
+    isAddableTarget(target) {
+        return target.my && super.isAddableTarget(target);
+    }
+    
+    newTarget(creep) {
+        if (creep.room.energyAvailable === creep.room.energyCapacityAvailable) {
+            return;
         }
-    });
-};
-action.work = function(creep){
-    let result = creep.transfer(creep.target, RESOURCE_ENERGY);
-    if (result == OK && creep.carry.energy > creep.target.energyCapacity-creep.target.energy) {
-        creep.target = null;
-        this.assign(creep);
+        return creep.pos.findClosestByRange(creep.room.structures.all, {
+            filter: s => {
+                return ((s instanceof StructureExtension || s instanceof StructureSpawn) &&
+                this.isValidTarget(s) && this.isAddableTarget(s, creep));
+            }
+        });
     }
-    return result;
+    
+    work(creep) {
+        let result = creep.transfer(creep.target, RESOURCE_ENERGY);
+        if (result === OK && creep.carry.energy > creep.target.energyCapacity - creep.target.energy) {
+            creep.target = null;
+            this.assign(creep);
+        }
+        return result;
+    }
+    
 };
-action.onAssignment = function(creep, target) {
-    //if( SAY_ASSIGNMENT ) creep.say(String.fromCharCode(9739), SAY_PUBLIC);
-    if( SAY_ASSIGNMENT ) creep.say(ACTION_SAY.FEEDING, SAY_PUBLIC);
-};
+module.exports = new action('feeding');
