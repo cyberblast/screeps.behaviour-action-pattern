bb8a5a74d428f28f5533e3faef44cd22938f090d
Spedwards <improbablepuzzle@gmail.com>
2017-04-05 17:15:52 +1000

    Action.Defending

--- creep.action.defending.js
+++ creep.action.defending.js
@@ -1,104 +1,97 @@
-let action = new Creep.Action('defending');
-module.exports = action;
-action.isValidAction = function(creep){ return creep.room.hostiles.length > 0; };
-action.isAddableAction = function(){ return true; };
-action.isAddableTarget = function(){ return true; };
-action.isValidTarget = function(target){
-    return (
-        target &&
-        target.hits != null &&
-        target.hits > 0 &&
-        target.my == false );
-};
-action.newTarget = function(creep){
-    var closestHostile = creep.pos.findClosestByRange(creep.room.hostiles, {
-        filter: creep.getStrategyHandler([action.name], 'priorityTargetFilter', creep)
-    });
-    if(!closestHostile) {
-        closestHostile = creep.pos.findClosestByRange(creep.room.hostiles, {
-            filter: creep.getStrategyHandler([action.name], 'targetFilter', creep)
+const action = class extends Creep.Action {
+    
+    constructor(...args) {
+        super(...args);
+        
+        this.statement = ACTION_SAY.DEFENDING;
+        
+        this.defaultStrategy.priorityTargetFilter = function(creep) {
+            return function(hostile) {
+                return true;
+            }
+        };
+        
+        this.defaultStrategy.targetFilter = function(creep) {
+            return function(hostile) {
+                return true;
+            }
+        }
+    }
+    
+    isValidAction(creep) {
+        return creep.room.hostiles.length > 0;
+    }
+    
+    isValidTarget(target) {
+        return super.isValidTarget(target) && target.hits !== null && target.hits > 0 && !target.my;
+    }
+    
+    newTarget(creep) {
+        let closestHostile = creep.pos.findClosestByRange(creep.room.hostiles, {
+            filter: creep.getStrategyHandle([action.name], 'priorityTargetFilter', creep)
         });
+        if (!closestHostile) {
+            closestHostile = creep.pos.findClosestByRange(creep.room.hostiles, {
+                filter: creep.getStrategyHandler([action.name], 'targetFilter', creep)
+            });
+        }
+        return closestHostile;
     }
-    return closestHostile;
-};
-action.step = function(creep){
-    if(CHATTY) creep.say(this.name, SAY_PUBLIC);
-    if (creep.target.pos.roomName !== creep.room.name) return Creep.action.travelling.assignRoom(creep, creep.target.pos.roomName);
-    this.run[creep.data.creepType](creep);
-};
-action.run = {
-    ranger: function(creep) {
-        let range = creep.pos.getRangeTo(creep.target);
-        if( !creep.flee ){
-            if( range > 3 ){
-                creep.travelTo(creep.target);
-                // FIXME: use custom costMatrix to implement COMBAT_CREEPS_RESPECT_RAMPARTS
-                // let path = creep.room.findPath(creep.pos, creep.target.pos, {ignoreCreeps: false});
-                // if( path && path.length > 0 ) {
-                //     let isRampart = COMBAT_CREEPS_RESPECT_RAMPARTS && _.some( creep.room.lookForAt(LOOK_STRUCTURES, path[0].x, path[0].y), {'structureType': STRUCTURE_RAMPART });
-                //     if(!isRampart){
-                //         creep.move(path[0].direction);
-                //     }
-                // } else {
-                //     // no path -> try to move by direction
-                //     let direction = creep.pos.getDirectionTo(creep.target);
-                //     if( direction ) creep.move(direction);
-                // }
+    
+    step(creep) {
+        if (CHATTY) creep.say(this.name, SAY_PUBLIC);
+        if (creep.target.pos.roomName !== creep.room.name) return Creep.action.travelling.assignRoom(creep, creep.target.pos.roomName);
+        this.run[creep.data.creepType](creep);
+    }
+    
+    get run() {
+        return {
+            ranger(creep) {
+                const range = creep.pos.getRangeTo(creep.target);
+                if (!creep.flee) {
+                    if (range > 3) {
+                        creep.travelTo(creep.target);
+                        // TODO: use custom costMatrix to implement COMBAT_CREEPS_RESPECT_RAMPARTS
+                    }
+                    if (range < 3) {
+                        const direction = creep.target.pos.getDirectionTo(creep);
+                        if (direction) creep.move(direction);
+                    }
+                }
+                
+                // attack ranged
+                const targets = creep.pos.findInRange(creep.room.hostiles, 3);
+                if (targets.length > 2) {
+                    // TODO: precalc damage dealt
+                    if (CHATTY) creep.say('MassAttack');
+                    creep.attackingRanged = creep.rangedMassAttack() === OK;
+                    return;
+                }
+                if (range < 4) {
+                    creep.attackingRanged = creep.rangedAttack(creep.target) === OK;
+                    return;
+                }
+                if (targets.length > 0) {
+                    creep.attackingRanged = creep.rangedAttack(targets[0]) === OK;
+                }
+            },
+            
+            melee(creep) {
+                if (!creep.flee) {
+                    creep.travelTo(creep.target);
+                    // TODO: need to set a custom costMatrix to respect ramparts
+                }
+                // attack
+                const attacking = creep.attack(creep.target);
+                if (attacking === ERR_NOT_IN_RANGE) {
+                    const targets = creep.pos.findInRange(creep.room.hostiles, 1);
+                    if (target.length > 0) creep.attacking = creep.attack(targets[0]) === OK;
+                } else {
+                    creep.attacking = attacking === OK;
+                }
             }
-            if( range < 3 ){
-                let direction = creep.target.pos.getDirectionTo(creep);
-                if( direction ) creep.move(direction);
-            }
         }
-
-        // attack ranged
-        let targets = creep.pos.findInRange(creep.room.hostiles, 3);
-        if(targets.length > 2) { // TODO: precalc damage dealt
-            if(CHATTY) creep.say('MassAttack');
-            creep.attackingRanged = creep.rangedMassAttack() == OK;
-            return;
-        }
-        if( range < 4 ) {
-            creep.attackingRanged = creep.rangedAttack(creep.target) == OK;
-            return;
-        }
-        if(targets.length > 0){
-            creep.attackingRanged = creep.rangedAttack(targets[0]) == OK;
-        }
-    },
-    melee: function(creep) {
-        if( !creep.flee ){
-            creep.travelTo(creep.target);
-            // FIXME: need to set a custom costMatrix to respect ramparts.
-            // let path = creep.room.findPath(creep.pos, creep.target.pos);
-            // // not standing in rampart or next step is rampart as well
-            // if( path && path.length > 0 && (
-            //     !COMBAT_CREEPS_RESPECT_RAMPARTS ||
-            //     !_.some( creep.room.lookForAt(LOOK_STRUCTURES, creep.pos.x, creep.pos.y), {'structureType': STRUCTURE_RAMPART } )  ||
-            //     _.some( creep.room.lookForAt(LOOK_STRUCTURES, path[0].x, path[0].y), {'structureType': STRUCTURE_RAMPART }))
-            // ){
-            //     creep.move(path[0].direction);
-            // }
-        }
-        // attack
-        let attacking = creep.attack(creep.target);
-        if( attacking == ERR_NOT_IN_RANGE ) {
-            let targets = creep.pos.findInRange(creep.room.hostiles, 1);
-            if( targets.length > 0)
-                creep.attacking = creep.attack(targets[0]) == OK;
-        } else creep.attacking = attacking == OK;
     }
+    
 };
-action.onAssignment = function(creep, target) {
-    if( SAY_ASSIGNMENT ) creep.say(ACTION_SAY.DEFENDING, SAY_PUBLIC);
-};
-action.defaultStrategy.priorityTargetFilter = function(creep) {
-    return function(hostile) {
-        return hostile.hasBodyparts(HEAL);
-    }
-};
-action.defaultStrategy.targetFilter = function(creep) {
-    return function(hostile) {
-        return true;
-    }
-};
+module.exports = new action('defending');
