0e5a468b07001c4c879275e96e87a930585334b4
Spedwards <improbablepuzzle@gmail.com>
2017-04-05 20:04:24 +1000

    Action.Dropping

--- creep.action.dropping.js
+++ creep.action.dropping.js
@@ -1,51 +1,50 @@
-let action = new Creep.Action('dropping');
-module.exports = action;
-action.targetRange = 1;
-action.reachedRange = 0;
-action.isValidAction = function(creep){
-    return creep.sum > 0;
-};
-action.newTarget = function(creep) {
-    // drop off at drop pile or the nearest spawn
-    let drop = creep.pos.findClosestByRange(creep.room.structures.piles);
-    if( !drop ) {
-        drop = creep.pos.findClosestByRange(creep.room.structures.spawns);
+const action = class extends Creep.Action {
+    
+    constructor(...args) {
+        super(...args);
+        
+        this.reachedRange = 0;
+        this.statement = ACTION_SAY.DROPPING;
     }
-    if( !drop ) {
-        drop = creep.pos.findClosestByRange(creep.room.find(FIND_FLAGS, FlagDir.flagFilter(FLAG_COLOR.claim.spawn)));
+    
+    isValidAction(creep) {
+        return creep.sum > 0;
     }
-    if (!drop) {
-        drop = creep.pos.findClosestByRange(_.filter(creep.room.constructionSites, {structureType: STRUCTURE_SPAWN}));
+    
+    newTarget(creep) {
+        // drop off at drop pile or the nearest spawn
+        return creep.pos.findClosestByRange(creep.room.structures.piles) ||
+                creep.pos.findClosestByRange(creep.room.structures.spawns) ||
+                creep.pos.findClosestByRange(creep.room.find(FIND_FLAGS, FlagDir.flagFilter(FLAG_COLOR.claim.spawn))) ||
+                creep.pos.findClosestByRange(_.filter(creep.room.constructionSites, {structureType: STRUCTURE_SPAWN})) ||
+                creep.room.controller ||
+                undefined;
     }
-    if (!drop) {
-        drop = creep.room.controller;
-    }
-    return drop;
-};
-action.work = function(creep) {
-    let ret = OK;
-    let isSpawnFlag = f => f && Flag.compare(f, FLAG_COLOR.claim.spawn);
-    if (!(creep.target instanceof StructureSpawn || creep.target instanceof ConstructionSite
-        || creep.target instanceof StructureController || isSpawnFlag(creep.target))) {
-        let range = creep.pos.getRangeTo(creep.target);
-        if( range > 0 && creep.data.lastPos && creep.data.path && !_.eq(creep.pos, creep.data.lastPos) ) {
-            // If the destination is walkable, try to move there before dropping
-            let invalidObject = o => {
-                return ((o.type == LOOK_TERRAIN && o.terrain == 'wall') ||
-                     o.type == LOOK_CREEPS ||
-                    (o.type == LOOK_STRUCTURES && OBSTACLE_OBJECT_TYPES.includes(o.structure.structureType) ));
-            };
-            let look = creep.room.lookAt(target);
-            if (!_.some(look, invalidObject)) {
-                return ret;
+    
+    work(creep) {
+        let ret = OK;
+        const isSpawnFlag = f => f && Flag.compare(f, FLAG_COLOR.claim.spawn);
+        if (!(creep.target instanceof StructureSpawn || creep.target instanceof ConstructionSite ||
+            creep.target instanceof StructureController || isSpawnFlag(creep.target))) {
+            let range = creep.pos.getRangeto(creep.target);
+            if (range > 0 && creep.data.lastPos && creep.data.path && !_.isEqual(creep.pos, creep.data.lastPos)) {
+                // If the destination is walkable, try to move there before dropping
+                const invalidObject = o => {
+                    return ((o.type === LOOK_TERRAIN && o.terrain === 'wall') ||
+                    o.type === LOOK_CREEPS ||
+                    (o.type === LOOK_STRUCTURES && OBSTACLE_OBJECT_TYPES.includes(o.structure.structureType)));
+                };
+                let look = creep.room.lookAt(target);
+                if (!_.some(look, invalidObject)) {
+                    return ret;
+                }
             }
         }
+        for (const resourceType in creep.carry) {
+            ret = creep.drop(resourceType);
+        }
+        return ret;
     }
-    for(let resourceType in creep.carry) {
-        ret = creep.drop(resourceType);
-    }
-    return ret;
+    
 };
-action.onAssignment = function(creep, target) {
-    if( SAY_ASSIGNMENT ) creep.say(ACTION_SAY.DROPPING, SAY_PUBLIC);
-};
+module.exports = new action('dropping');
