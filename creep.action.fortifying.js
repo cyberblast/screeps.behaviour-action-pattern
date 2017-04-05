b198736ed077c63e0b6e3ec754ff89bd7d08e1d3
Spedwards <improbablepuzzle@gmail.com>
2017-04-05 20:24:46 +1000

    Action.Fortifying

--- creep.action.fortifying.js
+++ creep.action.fortifying.js
@@ -1,22 +1,30 @@
-let action = new Creep.Action('fortifying');
-module.exports = action;
-action.maxPerTarget = 1;
-action.maxPerAction = 1;
-action.targetRange = 3;
-action.isValidAction = function(creep){
-    return (creep.carry.energy > 0 && ( !creep.room.storage || creep.room.storage.charge > 0.6 ));
+const action = class extends Creep.Action {
+    
+    constructor(...args) {
+        super(...args);
+        
+        this.maxPerTarget = 1;
+        this.maxPerAction = 1;
+        this.targetRange = 3;
+        this.statement = ACTION_SAY.FORTIFYING;
+    }
+    
+    isValidAction(creep) {
+        return creep.carry.energy > 0 && (!creep.room.storage || creep.room.storage.charge > 0.6);
+    }
+    
+    isValidTarget(target) {
+        return super.isValidTarget(target) && target.hits && target.hits < target.hitsMax;
+    }
+    
+    newTarget(creep) {
+        const isAddable = target => this.isAddableTarget(target, creep);
+        return _.find(creep.room.structures.fortifyable, isAddable);
+    }
+    
+    work(creep) {
+        return creep.repair(creep.target);
+    }
+    
 };
-action.isValidTarget = function(target){
-    return ( target != null && target.hits && target.hits < target.hitsMax);
-};
-action.newTarget = function(creep){
-    var that = this;
-    var isAddable = target => that.isAddableTarget(target, creep);
-    return _.find(creep.room.structures.fortifyable, isAddable);
-};
-action.work = function(creep){
-    return creep.repair(creep.target);
-};
-action.onAssignment = function(creep, target) {
-    if( SAY_ASSIGNMENT ) creep.say(ACTION_SAY.FORTIFYING, SAY_PUBLIC);
-};
+module.exports = new action('fortifying');
