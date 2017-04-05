22516eaae6f7f5c706dd060455dddcd7c37c9766
Spedwards <improbablepuzzle@gmail.com>
2017-04-05 17:25:08 +1000

    Action.Dismantling

--- creep.action.dismantling.js
+++ creep.action.dismantling.js
@@ -1,55 +1,66 @@
-let action = new Creep.Action('dismantling');
-module.exports = action;
-action.maxPerAction = 3;
-action.maxPerTarget = 3;
-action.renewTarget = false;
-action.isValidAction = function(creep){
-    return creep.sum < creep.carryCapacity;
-};
-action.isValidTarget = function(target){
-    return target != null;
-};
-action.newTarget = function(creep){
-    let target;
-    let flag = FlagDir.find(FLAG_COLOR.destroy.dismantle, creep.pos, true);
-    if( flag ){
-        if( flag.room !== undefined ){ // room is visible
-            var targets = flag.room.lookForAt(LOOK_STRUCTURES, flag.pos.x, flag.pos.y);
-            if( targets && targets.length > 0)
-                return targets[0];
-            else { // remove flag. try next flag
-                let oldName = flag.name;
-                FlagDir.removeFromDir(flag.name);
-                flag.remove();
-
-                let otherFlagMod = (range, flagItem, args) => {
-                    if(flagItem.name == args) return Infinity;
-                    return range;
-                };
-                flag = FlagDir.find(FLAG_COLOR.destroy.dismantle, creep.pos, true, otherFlagMod, oldName);
-                if( oldName == flag.name ) logError('Removed flag found again in dismantling.newTarget!');
-                if( flag ){
-                    if( flag.room !== undefined ){ // room is visible
-                        var targets = flag.room.lookForAt(LOOK_STRUCTURES, flag.pos.x, flag.pos.y);
-                        if( targets && targets.length > 0)
-                            return targets[0];
-                        else { // remove flag. try next flag
-                            Room.costMatrixInvalid.trigger(flag.room);
-                            FlagDir.removeFromDir(flag.name);
-                            flag.remove();
+const action = class extends Creep.Action {
+    
+    constructor(...args) {
+        super(...args);
+        
+        this.maxPerAction = 3;
+        this.maxperTarget = 3;
+        this.renewTarget = false;
+        this.statement = ACTION_SAY.DISMANTLING;
+    }
+    
+    isValidAction(creep) {
+        return creep.sum < creep.carryCapacity;
+    }
+    
+    newTarget(creep) {
+        let target;
+        let flag = FlagDir.find(FLAG_COLOR.destroy.dismantle, creep.pos);
+        if (flag) {
+            if (flag.room) {
+                // room is visible
+                let targets = flag.pos.lookFor(LOOK_STRUCTURES);
+                if (targets && targets.length > 0) {
+                    return targets[0];
+                } else {
+                    // remove flag. try next flag
+                    let oldName = flag.name;
+                    FlagDir.removeFromDir(flag.name);
+                    flag.remove();
+                    
+                    const otherFlagMod = (range, flagItem, args) => {
+                        if (flagItem.name === args) return Infinity;
+                        return range;
+                    };
+                    flag = FlagDir.find(FLAG_COLOR.destroy.dismantle, creep.pos, true, otherFlagMod, oldName);
+                    if (oldName === flag.name) logError('Removed flag found again in dismantling.newTarget!');
+                    if (flag) {
+                        if (flag.room) {
+                            // room is visible
+                            const targets = flag.pos.lookFor(LOOK_STRUCTURES);
+                            if (targets && targets.length > 0) {
+                                return targets[0];
+                            } else {
+                                // remove flag. try next flag
+                                Room.costMatrixInvalid.trigger(flag.room);
+                                Flag.removeFromDir(flag.name);
+                                flag.remove();
+                            }
+                        } else {
+                            target = flag;
                         }
                     }
-                    else target = flag; // target in other room
                 }
+            } else {
+                target = flag;
             }
         }
-        else target = flag; // target in other room
+        return target;
     }
-    return target;
+    
+    work(creep) {
+        return creep.dismantle(creep.target);
+    }
+    
 };
-action.work = function(creep){
-    return creep.dismantle(creep.target);
-};
-action.onAssignment = function(creep, target) {
-    if( SAY_ASSIGNMENT ) creep.say(ACTION_SAY.DISMANTLING, SAY_PUBLIC);
-};
+module.exports = new action('dismantling');
