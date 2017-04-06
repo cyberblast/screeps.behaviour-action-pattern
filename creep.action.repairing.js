2a95af993716ae654ca96575dec6f3481fb5d366
Spedwards <improbablepuzzle@gmail.com>
2017-04-07 04:08:56 +1000

    Action.Repairing

--- creep.action.repairing.js
+++ creep.action.repairing.js
@@ -1,35 +1,40 @@
-let action = new Creep.Action('repairing');
-module.exports = action;
-action.targetRange = 3;
-action.maxPerTarget = 1;
-action.isValidAction = function(creep){
-    return (creep.carry.energy > 0 );
-};
-action.isValidTarget = function(target){
-    return ( target != null && target.hits &&
-    target.hits < target.hitsMax);
-};
-action.isAddableTarget = function(target, creep){
-    return (
-        (target instanceof OwnedStructure && target.my) ||
-        (
-            (!creep.room.controller ||
+const action = class extends Creep.Action {
+    
+    constructor(...args) {
+        super(...args);
+    
+        this.maxPerTarget = 1;
+        this.targetRange = 3;
+    }
+    
+    isValidAction(creep) {
+        return creep.carry.energy > 0;
+    }
+    
+    isValidTarget(target) {
+        return super.isValidTarget(target) && target.hits && target.hits < target.hitsMax;
+    }
+    
+    isAddableTarget(target, creep) {
+        return (
+            (target instanceof OwnedStructure && target.my) ||
+            (
+                (!creep.room.controller ||
                 (
                     (!creep.room.controller.owner || creep.room.controller.my) &&
-                    (!creep.room.controller.reservation || creep.room.controller.reservation.username == creep.owner.username)
-                )
+                    (!creep.room.controller.reservation || creep.room.controller.reservation.username === creep.owner.username)
+                ))
             )
-        )
-    ) && (!target.targetOf || target.targetOf.length < this.maxPerTarget);
+        ) && super.isAddableTarget(target, creep);
+    }
+    
+    newTarget(creep) {
+        return _.find(creep.room.structures.urgentRepairable, target => this.isAddableTarget(target, creep));
+    }
+    
+    work(creep) {
+        return creep.repair(creep.target);
+    }
+    
 };
-action.newTarget = function(creep){
-    var that = this;
-    var isAddable = target => that.isAddableTarget(target, creep);
-    return _.find(creep.room.structures.urgentRepairable, isAddable);
-};
-action.work = function(creep){
-    return creep.repair(creep.target);
-};
-action.onAssignment = function(creep, target) {
-    if( SAY_ASSIGNMENT ) creep.say(ACTION_SAY.REPAIRING, SAY_PUBLIC);
-};
+module.exports = new action('repairing');
