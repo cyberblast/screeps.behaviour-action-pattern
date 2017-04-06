825f0b3cb6dd76daeddb65fae9821eec68d65614
Spedwards <improbablepuzzle@gmail.com>
2017-04-07 04:23:13 +1000

    Action.Reserving

--- creep.action.reserving.js
+++ creep.action.reserving.js
@@ -1,65 +1,57 @@
-let action = new Creep.Action('reserving');
-module.exports = action;
-action.isValidAction = function(creep){ return true; };
-action.isValidTarget = function(target){  return target && (!target.reservation || target.reservation.ticksToEnd < 4999 ) };
-action.isAddableAction = function(){ return true; };
-action.isAddableTarget = function(target, creep){ return target &&
-    ( target instanceof Flag || ( target.structureType === 'controller' && !target.owner ) );
-};
-action.newTarget = function(creep){
-    let validColor = flagEntry => (
-        Flag.compare(flagEntry, FLAG_COLOR.claim.reserve) || Flag.compare(flagEntry, FLAG_COLOR.invade.exploit)
-    );
+const action = class extends Creep.Action {
 
-    let flag;
-    // TODO: remove  || creep.data.destiny.flagName (temporary backward compatibility)
-    if( creep.data.destiny ) flag = Game.flags[creep.data.destiny.targetName || creep.data.destiny.flagName];
-    if ( !flag ) flag = FlagDir.find(validColor, creep.pos, false, FlagDir.reserveMod, creep.name);
-
-    if( flag ) {
+    isValidTarget(target) {
+        return super.isValidTarget(target) && (!target.reservation || target.reservation.ticksToEnd < 4999);
+    }
+    
+    isAddableTarget(target, creep) {
+        return !!target && (target instanceof Flag || (target instanceof StructureController && !target.owner));
+    }
+    
+    newTarget(creep) {
+        const validColour = flagEntry => Flag.compare(flagEntry, FLAG_COLOR.claim.reserve) || Flag.compare(flagEntry, FLAG_COLOR.invade.exploit);
+        
+        let flag;
+        // TODO: remove || creep.data.destiny.flagName (temporary backward compatibility)
+        if (creep.data.destiny) flag = Game.flags[creep.data.destiny.targetName || creep.data.destiny.flagName];
+        if (!flag) flag = FlagDir.find(validColour, creep.pos, false, FlagDir.reserveMod, creep.name);
+        
+        if (!flag) return;
+        
         Population.registerCreepFlag(creep, flag);
+        
+        // not there, go to flagged room
+        if (!creep.flag.room || creep.flag.pos.roomName !== creep.pos.roomName) return creep.flag;
+        
+        return creep.flag.room.controller;
     }
-    else return null;
-
-    // not there, go to flagged room
-    if( !creep.flag.room || creep.flag.pos.roomName != creep.pos.roomName){
-        return creep.flag;
-    }
-
-    return creep.flag.room.controller;
-};
-
-action.step = function(creep){
-    if(CHATTY) creep.say(this.name, SAY_PUBLIC);
-    if( creep.target.color ){ 
-        if( creep.flag.pos.roomName == creep.pos.roomName ) // change target from flag to controller
-            creep.data.targetId = null;
-        return creep.travelTo( creep.target.pos );
-    }
-
-    let range = creep.pos.getRangeTo(creep.target);
-    if( range <= this.targetRange ) {
-        var workResult = this.work(creep);
-        if( workResult != OK ) {
-            creep.handleError({errorCode: workResult, action: this, target: creep.target, range, creep});
+    
+    step(creep) {
+        if (CHATTY) creep.say(this.name, SAY_PUBLIC);
+        if (creep.target instanceof Flag) {
+            if (creep.flag.pos.roomName === creep.pos.roomName) delete creep.data.targetId; // change target from flag to controller
+            return creep.travelTo(creep.target.pos);
         }
-        return workResult;
+        
+        const range = creep.pos.getRangeTo(creep.target);
+        if (range <= this.targetRange) {
+            const workResult = this.work(creep);
+            if (workResult !== OK) {
+                creep.handleError({errorCode: workResult, action: this, target: creep.target, range, creep});
+            }
+            return workResult;
+        }
+        return creep.travelTo(creep.target.pos);
     }
-    return creep.travelTo( creep.target.pos );
-};
-action.work = function(creep){
-    var workResult;
+    
+    work(creep) {
+        creep.controllerSign();
+        
+        const work = creep.target.owner && !creep.target.my ? creep.attackController : creep.reserveController;
+        
+        return work.call(creep, creep.target);
+    }
+    
 
-    creep.controllerSign();
-
-    if( creep.target.owner && !creep.target.my ){
-        workResult = creep.attackController(creep.target);
-    }
-    else {
-        workResult = creep.reserveController(creep.target);
-    }
-    return workResult;
 };
-action.onAssignment = function(creep, target) {
-    if( SAY_ASSIGNMENT ) creep.say(ACTION_SAY.RESERVING, SAY_PUBLIC);
-};
+module.exports = new action('reserving');
